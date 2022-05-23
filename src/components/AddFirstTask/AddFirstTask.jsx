7bf4c9f4c4df60758be257f28c2fbb4adf29298e
RakshithBhat03 <rakshith6666@gmail.com>
2022-05-24 00:35:33 +0530

    [feat]: add task functionality

--- src/components/AddFirstTask/AddFirstTask.jsx
+++ src/components/AddFirstTask/AddFirstTask.jsx
@@ -1,12 +1,22 @@
 import React from "react";
+import { Modal } from "../Modal/Modal";
+import { useModal } from "../../context";
 import "./AddFirstTask.css";
+import { deleteLocalStorage } from "../../utils/localStorage";
 function AddFirstTask() {
+  const { modal, setModal } = useModal();
   return (
     <div className="first-task-wrapper display-flex flex-col justify-content-center align-items-center gap-1">
       <h2 className="txt-white m-0">Add Task</h2>
-      <button className="txt-white txt-lg btn-task-large">
+      <button
+        onClick={() => {
+          deleteLocalStorage("EditTask");
+          setModal((modal) => !modal);
+        }}
+        className="txt-white txt-lg btn-task-large">
         <i className="fas fa-plus-circle"></i>
       </button>
+      {modal && <Modal />}
     </div>
   );
 }
