305d24e938ba4ef7b31f47a22fc5bec7a247e7c7
RakshithBhat03 <rakshith6666@gmail.com>
2022-05-24 00:38:02 +0530

    [feat]: added showAllTasks component

--- src/pages/Tasks/Tasks.jsx
+++ src/pages/Tasks/Tasks.jsx
@@ -1,14 +1,24 @@
-import React from "react";
-import { AddFirstTask } from "../../components";
+import React, { useEffect } from "react";
+import { ShowAllTasks, AddFirstTask } from "../../components/";
+import { useTask } from "../../context";
+import { setLocalStorage } from "../../utils/localStorage";
+
 import "./Tasks.css";
-import { useDocumentTitle } from "../../hooks";
 function Tasks() {
-  useDocumentTitle("Tasks | Buzzer Beater");
+  const { task } = useTask();
+  useEffect(() => {
+    setLocalStorage("task", task);
+  });
   return (
     <div className="tasks-wrapper mx-auto display-flex flex-col p-9">
       <h2 className="txt-white my-5 mx-0">Welcome back user!</h2>
+      <h3 className="txt-white my-5 mx-0">
+        {task.length === 0
+          ? "Add a task to get started"
+          : "You have some tasks"}
+      </h3>
       <div className="tasks-container">
-        <AddFirstTask />
+        {task.length === 0 ? <AddFirstTask /> : <ShowAllTasks />}
       </div>
     </div>
   );
