333c05f5619ac165a7a8bf0c195c8ffb18c49c2b
RakshithBhat03 <rakshith6666@gmail.com>
2022-05-24 13:46:47 +0530

    [refactor]: added task handler

--- src/components/AddFirstTask/AddFirstTask.jsx
+++ src/components/AddFirstTask/AddFirstTask.jsx
@@ -5,14 +5,15 @@ import "./AddFirstTask.css";
 import { deleteLocalStorage } from "../../utils/localStorage";
 function AddFirstTask() {
   const { modal, setModal } = useModal();
+  const addTaskHandler = () => {
+    deleteLocalStorage("EditTask");
+    setModal((modal) => !modal);
+  };
   return (
     <div className="first-task-wrapper display-flex flex-col justify-content-center align-items-center gap-1">
       <h2 className="txt-white m-0">Add Task</h2>
       <button
-        onClick={() => {
-          deleteLocalStorage("EditTask");
-          setModal((modal) => !modal);
-        }}
+        onClick={addTaskHandler}
         className="txt-white txt-lg btn-task-large">
         <i className="fas fa-plus-circle"></i>
       </button>
